517674f791713616db858d73cff44dfe840a6127
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-02-06 22:13:05 +0600

    es6 practise inheritance

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+// const doubleIt = function(num){
+//     return num * 2;
+// }
+
+// const doubleIt = function myFun(num){
+//     return num * 2;
+// }
+
+// es6
+const doubleIt = num => num *2;
+const add = (x,y) => x + y;
+const give5 = () => 5;
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x-y;
+    const result = sum * diff;
+    return result;
+}
+const result = add(5,6);
+const resultDouble = doubleIt(5);
+const giveFive = give5();
+const resultDoMath = doMath();
+console.log(result);
+console.log(giveFive);
+console.log(resultDoMath);
---
+++ class.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Kalimunnasa School";
+    }
+
+}
+const student1 = new Student(12, "Shuvo");
+const student2 = new Student(22, "Mahiya");
+console.log(student1, student2);
+console.log(student1.name, student2.name);
